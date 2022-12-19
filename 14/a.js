3d2eb6959e42ed85f4fe6206376cbcdd4973f0a1
LoufaSupreme <josh.davis@live.ca>
2022-12-18 22:18:26 -0500

    finished 14 a

--- 14/a.js
+++ 14/a.js
@@ -39,11 +39,16 @@ function populateMap(lines) {
 
     return Array.from(map).map(pt => {
         [x,y] = pt.split(',').map(Number);
-        return {x, y};
+        return {x, y, type: 'wall'};
     });
 }
 
-function drawMap(map, sandOrigin) {
+function hitWall(map, target) {
+    return map.find(point => point.x === target.x && point.y === target.y);
+}
+
+
+function drawMap(map) {
     let minX = Infinity;
     let minY = Infinity;
     let maxX = -Infinity;
@@ -60,33 +65,96 @@ function drawMap(map, sandOrigin) {
     const grid = Array(maxY-minY+1+mapPadding*2).fill(0).map(elem => Array(maxX-minX+1+mapPadding*2).fill(' '));
     for (let row = 0; row < grid.length; row++) {
         for (let col = 0; col < grid[0].length; col++) {
-            isVal = map.find(elem => {
-                return elem.x === col + minX - mapPadding && elem.y === row + minY - mapPadding;
-            });
+            
             if (row === grid.length - 1) grid[row][col] = '_';
-            if (col === 0 || col === grid[0].length - 1) grid[row][col] = '|'
-            if (isVal) {
-                grid[row][col] = '🧱';
-            }
-            if (sandOrigin.x === col+minX-mapPadding && sandOrigin.y === row+minY-mapPadding) {
-                grid[row][col] = "+"
+            if (col === 0 || col === grid[0].length - 1) grid[row][col] = '|';
+
+            const onMap = hitWall(map, {x: col + minX - mapPadding, y: row + minY - mapPadding});
+            if (onMap) {
+                if (onMap.type === 'wall') grid[row][col] = '🧱';
+                else if (onMap.type === 'sand') grid[row][col] = 'o';
+                else if (onMap.type === 'Sand Origin') grid[row][col] = "+"
             }
         }
     }
 
     const drawnMap = grid.map(line => line.join('')).join('\n');
     fs.writeFileSync('map.txt', drawnMap);
+    return maxY;
+}
+
+function isBlocked(map, pos) {
+    // console.log(map)
+    if (!hitWall(map, pos)) return false;
+    else if (hitWall(map, {x: pos.x - 1, y: pos.y}) && hitWall({x: pos.x + 1, y: pos.y})) return true;
+    return false;
+}
+
+function isOnMap(lowestPoint, pos) {
+    return pos.y <= lowestPoint;
+}
+
+function createSandParticle(map, sandOrigin, lowestPoint) {
+    const sand = {
+        x: sandOrigin.x,
+        y: sandOrigin.y,
+        type: 'sand',
+        atRest: false
+    };
+
+    while (!sand.atRest) {
+        if (!isOnMap(lowestPoint, sand)) return;
+        // if we can move down w/o hitting a wall, continue
+        if (!hitWall(map, {x: sand.x, y: sand.y+1})) sand.y += 1;
+        // if we'll hit a wall on the next move:
+        else {
+            // check if we can go left
+            if (!hitWall(map, {x: sand.x-1, y: sand.y+1})) {
+                sand.x -= 1;
+                sand.y += 1;
+            }
+            // check if we can go right
+            else if (!hitWall(map, {x: sand.x+1, y: sand.y+1})) {
+                sand.x += 1;
+                sand.y += 1;
+            }
+            // if we can't go anywhere then the sand stops
+            else {
+                sand.atRest = true;
+                return sand;
+            }
+        }
+    }
 }
 
 function main() {
     const sandOrigin = {
         x: 500,
-        y: 0
+        y: 0, 
+        type: 'Sand Origin'
     }
     const points = parseInput(input);
-    const map = [...populateMap(points), sandOrigin];
+    let map = [...populateMap(points), sandOrigin];
     // console.log(map)
-    drawMap(map, sandOrigin)
+    const lowestPoint = drawMap(map);
+
+    let complete = false;
+    let sandParticles = 0;
+    while (!complete) {
+    // for (let i = 0; i < 25; i++) {
+        const sand = createSandParticle(map, sandOrigin, lowestPoint);
+        if (sand) {
+            map = [...map, sand];
+            sandParticles++;
+            // console.log(sand)
+            // console.log(map)
+        }
+        else {
+            complete = true;
+        }
+    }
+    console.log(sandParticles)
+    drawMap(map);
 }
 
 // console.log(parseInput(input));
